26b08c62ef5a2f2aafa60850c3610cd1ba9becdb
Carl Euca <business.carl.euca@gmail.com>
2025-03-29 22:52:38 +0000

    Added game over scene
    Created GameOver scene, added GameOverMenu folder and modified MenuAlt scene game title

---
+++ Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Action");
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
