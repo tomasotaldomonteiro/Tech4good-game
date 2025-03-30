de143807a6f16b15dd7122ffd25cd30e278b7a43
SaraSantos9 <ssarassantos9@gmail.com>
2025-03-30 11:40:11 +0000

    add sprites ao prefab enemie bullet and ally

---
+++ Assets/Scenes/Death menu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deathmenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+   void Quit(){
+
+
+   }
+}
